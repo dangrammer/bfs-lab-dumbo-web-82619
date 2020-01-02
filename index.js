792d01d31585c7df341603c0cf85d1318957ebbd
dangrammer <danieljromans@gmail.com>
2020-01-02 15:25:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-function bfs(rootNode, vertices, edges){
+function bfs(startingNode, vertices, edges){
+  startingNode.distance = 0
+  let discoveredNodes = [startingNode]
+  let nodeQueue = [startingNode]
+  while (discoveredNodes.length !== 0) {
+    let currentNode = discoveredNodes.shift()
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+    nodeQueue = nodeQueue.concat(adjacentNodes)
+    markDistanceAndPredecessor(currentNode, adjacentNodes)
+    discoveredNodes = discoveredNodes.concat(adjacentNodes)
+  }
+  return nodeQueue
+}
 
+function findAdjacent(nodeName, vertices, edges) {
+  let edgeList = edges.filter(edge => edge.includes(nodeName))
+  let nameList = [].concat.apply([], edgeList)
+  let targets = nameList.filter(name => name !== nodeName)
+  let adjacents = vertices.filter(vertex => targets.includes(vertex.name))
+
+  return adjacents.filter(vertex => vertex.distance === null)
+}
+
+function markDistanceAndPredecessor(predecessor, adjacentNodes) {
+  adjacentNodes.map(node => {
+    node.distance = predecessor.distance + 1;
+    node.predecessor = predecessor;
+  })
 }
